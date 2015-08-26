2913c8315f12cacf5b51dd27f9019b1e429e0809
Nguyen Hoang Long <roman.long@email.cz>
2015-08-26 18:17:24 +0200

    merging answers divided by newline

--- google_query.py
+++ google_query.py
@@ -11,11 +11,17 @@ class MyHTMLParser(HTMLParser):
         self.recording = False
         self.nested_recording = False
         self.found_bold = False
+        self.first = False
+        self.merge = False
         self.result = []
     def handle_starttag(self, tag, attrs):
         if self.recording == True and tag == 'b':
             self.found_bold = True
             self.result.append('b')
+        if tag == 'span' and self.first == True:
+            self.merge = True
+        if tag == 'span' and self.nested_recording == True:
+            self.first = True
         if tag == 'span':
             for name, value in attrs:           #extracted using blood, sweat and tears
                 if name == 'class' and value in ["_Tgc", "_Tfc", "_m3b", "_Oke", "_Xbe", "_Nbe"]:
@@ -26,11 +32,16 @@ class MyHTMLParser(HTMLParser):
                     self.nested_recording = True
     def handle_endtag(self, tag):
         if tag == 'span':
+            if self.merge == True:
+                self.merge = False                
             self.recording = False
-            self.nested_recording = False
         if tag == 'div' and self.nested_recording == True:
             self.nested_recording = False
+            self.first = False
     def handle_data(self, data):
+        if self.merge == True:
+            self.result[-1] = self.result[-1] + data 
+            return
         if self.recording == True:
             self.result.append(data)
         if self.nested_recording == True:
@@ -100,3 +111,5 @@ def loadFromFile(filename):
             return parseBoldStrings(parser.result)
         else:
             return parser.result
+
+if __name__ == "__main__": print(loadFromFile("tmp.log"))
